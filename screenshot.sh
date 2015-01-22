1fa2ce1f9b30eede69f763b7218186c1326d5c49
brownman <brownman556@gmail.com>
2015-01-22 18:51:11 +0200

    [travis] - curl should continue unless find gold

--- screenshot.sh
+++ screenshot.sh
@@ -1,10 +1,14 @@
+#!/usr/bin/env bash
+
 set +e
 url=${1:-'http://google.com'}
 
 while :;do
-curl 0.0.0.0:3000/screenshot?url=$url &>/dev/null && break
+curl 0.0.0.0:3000/screenshot?url=$url 1>/tmp/out 2>/tmp/err && break || true
 sleep 1;
 done
 
-echo good bye :)
+cat /tmp/out
+cat /tmp/err
 
+echo good bye!
