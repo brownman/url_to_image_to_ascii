69227e4d930ab6ca9ca4c254e44d756d8e02a3f4
brownman <brownman556@gmail.com>
2015-01-22 16:52:01 +0200

    [ console.log ] + convert the image to ascii-art

--- controllers/screenshot.js
+++ controllers/screenshot.js
@@ -1,4 +1,8 @@
 var screenshot = require('url-to-screenshot');
+var ascii = require('image-to-ascii');
+
+var fs = require('fs');
+
 
 exports.capture = function(req, res) {
   screenshot(req.query.url)
@@ -8,6 +12,15 @@ exports.capture = function(req, res) {
   .capture(function(err, img) {
     if (err) throw err;
     res.end(img, 'binary');
+
+   	fs.writeFileSync('/tmp/screenshot.png', img);
+
+
+//,colored:true,size:{height:50,width:50}
+	ascii({path: '/tmp/screenshot.png' }, function(err, converted) {
+	    console.log(err || converted);
+	});
+
   });
 
 }
---
+++ screenshot.sh
@@ -0,0 +1,4 @@
+
+url=${1:-'http://google.com'}
+curl 0.0.0.0:3000/screenshot?url=$url
+
