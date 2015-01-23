28c22446efb685b91039bcbfe657abe9bbf216d2
brownman <brownman556@gmail.com>
2015-01-23 19:58:28 +0200

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -6,11 +6,14 @@ url=${1:-'http://google.com'}
 
 while :;do
 curl 0.0.0.0:3000/screenshot?url=$url 1>/tmp/out 2>/tmp/err && break || true
-cat /tmp/out
-cat /tmp/err
+
 sleep 1;
 done
-
-
+echo /tmp/out
+echo ------------
+head -1 /tmp/out
+echo /tmp/err
+echo ------------
+head -1 /tmp/err
 
 echo good bye!
