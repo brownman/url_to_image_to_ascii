f1f8f7e013082ccbaa4d1a10de3fec5507f129a4
brownman <brownman556@gmail.com>
2015-01-24 18:21:29 +0200

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -2,8 +2,9 @@
 
 node server.js &
 sleep 7
-#http://google.com
-url=${1:-'http://dph.am/jscii/test/image.jpg'}
+url0='http://google.com'
+url1=''http://dph.am/jscii/test/image.jpg''
+url=${1:-\'$url0\'}
 
 while :;do
 curl 0.0.0.0:3000/screenshot?url=$url 1>/tmp/out 2>/tmp/err && break || true
