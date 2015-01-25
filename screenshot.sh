41cef1786b49c51f83ed19be32b298fb91f6059c
brownman <brownman556@gmail.com>
2015-01-25 12:00:30 +0200

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -11,11 +11,11 @@ curl 0.0.0.0:3000/screenshot?url=$url 1>/tmp/out 2>/tmp/err && break || true
 
 sleep 1;
 done
-echo /tmp/out
+#echo /tmp/out
+#echo ------------
+#head -1 /tmp/out
+test -f /tmp/err && { echo /tmp/err
 echo ------------
-head -1 /tmp/out
-echo /tmp/err
-echo ------------
-head -1 /tmp/err
+head -1 /tmp/err; } 
 
 echo good bye!
